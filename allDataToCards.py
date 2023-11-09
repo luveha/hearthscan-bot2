bfdeaa92fcccc010566ba5b424fe3247323be305
luveha <54841459+luveha@users.noreply.github.com>
2023-11-09 18:38:19 +0100

    Add files via upload

--- allDataToCards.py
+++ allDataToCards.py
@@ -5,77 +5,65 @@ cards = loadJson('data/allData')
 constants = loadJson('data/constants')
 
 def classConvert(i):
-    try:
-        classes = i['multiClassGroup'].split('/')    
-        for h in constants['classes']:
-            z = 0
-            while z < len(classes):
-                if classes[z].lower() == h.lower():
-                    classes[z] = constants['classes'][h]
-                z += 1
-        printString = ""
-        for v in classes:
-            printString += v + "/"
-        return printString.rpartition('/')[0]
-    except Exception as e:
-        try:
-            for h in constants['classes']:
-                if i['playerClass'].lower() == h.lower():
-                    return constants['classes'][h]
-        except Exception as e:
-            return "null"
-def subtype(i):
-    try:
-        string = i['race'] + "/" + i['otherRaces'][0] 
-        return string
-    except Exception as e:
-        try:
-            string = i['race'] 
-            return string
-        except Exception as e:
-            try:
-                string = i['spellSchool']
-                return string
-            except Exception as e:
-                return "null"
-def nullTest(i,v):
-    try:
-        m = i[v]
-        return m
-    except Exception as e:
+    classes = i.get('multiClassGroup', None)
+    class1 = i.get('playerClass', None)
+    if classes != None:
+        classes = classes.split('/')
+        for h in classes:
+            if not (constants['classes'].get(h) is None):
+                classes[classes.index(h)] = constants['classes'].get(h)
+        return '/'.join(classes)
+    elif not(constants['classes'].get(class1) is None):
+        return constants['classes'].get(class1)
+    else:
         return "null"
-def rarityTest(i):
-    try:
-        string = i['rarity']
-        return string
-    except Exception as e:
-        try:
-            if i['elite'] == True:
-                string = "elite"
-                return string
-        except Exception as e:
-            return "null"
-    
+
+def subtype(i):
+    race = i.get('race',None)
+    otherRace = i.get('otherRaces',None)
+    spellSchool = i.get('spellSchool',None)
+    if otherRace != None:
+        return race + '/' + '/'.join(otherRace)
+    elif race != None:
+        return race
+    elif spellSchool != None:
+        return spellSchool
+    else:
+        return "null"
+
+def nullTest(i,v):
+    return i.get(v, "null")
+
+def collectableTest(i):
+    if i.get("collectible", "null") != "null":
+        return True
+    else:
+        return False
+
+
 def cleanUp(i,v):
     try:
-        if nullTest(i,v) == "null":
+        if nullTest(i, v) == "null":
             return "null"
-        to_remov = {'\\n': ' ','<i>': '','</i>': '','[x]': '','<b>': '', '</b>': '','{0}': '','{1}': '','"': "'",'@ ( left!)':'',"@ (Ready!)":'','$':'','\\': ''}
+        to_remove = {'\\n': ' ', '<i>': '', '</i>': '', '[x]': '', '<b>': '', '</b>': '', '{0}': '', '{1}': '', '"': "'", '@ ( left!)': '', "@ (Ready!)": '', '$': '', '\\': '', '_': ' ',
+                    '</I>':'','   ': ' ','  ':' ','@ |4(copy, copies)':'1 copy'}
         string = i[v]
-        for char in to_remov.keys():
-            string = string.replace(char,to_remov[char])
+        for char in to_remove:
+            string = string.replace(char, to_remove[char])
         return string
     except Exception as e:
-            return "null"
+        return "null"
+
 def durORhp(i):
     try:
-        if nullTest(i,'health') != "null":
+        if 'health' in i:
             return i['health']
-        elif nullTest(i,'durability') != "null":
+        elif 'durability' in i:
             return i['durability']
         return "null"
     except Exception as e:
-            return "null"
+        return "null"
+
 def toImage(i):
     try:
         start = "https://cards.hearthpwn.com/enUS/"
@@ -87,23 +75,23 @@ def toImage(i):
 x = '{ }'
 z = json.loads(x)
 v = []
-#Removing things like prologue etc.
+
+#Removing things like adventure cards.
 remove = ['prologue']
+
 #Gets short hands from sets
-sets = []
-for h in constants['sets']:
-    sets.append(constants['sets'][h]['code'])
+sets = [constants['sets'][h]['code'] for h in constants['sets']]
+    
 #Gets full names from sets
-names = []
-for h in constants['sets']:
-    names.append(constants['sets'][h]['name'])
+names = [constants['sets'][h]['name'] for h in constants['sets']]
+
 #Running the test
 for k in cards:
     if k in names:
         for i in cards[k]:
             try:
                 if i['cardId'].split('_')[1].lower() not in remove:
-                    if i['type'] != "Enchantment" and i['cardId'].split('_')[0].upper() in sets:
+                    if i['type'] != "Enchantment" and (i['cardId'].split('_')[0].upper() in sets or i['cardSet'] in {'Core','Legacy'}):
                         dictionary = {
                             "id": f"{nullTest(i,'cardId')}",
                             "name": f"{nullTest(i,'name')}",
@@ -116,9 +104,16 @@ for k in cards:
                             "subType": f"{subtype(i)}",
                             "image": f"{toImage(i['cardId'])}",
                             "set": f"{i['cardSet']}",
-                            "rarity": f"{rarityTest(i)}"
+                            "rarity": f"{nullTest(i,'rarity')}",
+                            "collectable": f"{collectableTest(i)}"
                         }
-                        z[f"{i['name'].lower()}"] = dictionary
+                        if i['name'].lower() not in z:
+                            z[i['name'].lower()] = dictionary
+                        if i['cardSet'] == 'Core':
+                            z[f"{i['name'].lower()} ({z[i['name'].lower()]['set'].lower()})"] = z[i['name'].lower()]
+                            z[i['name'].lower()] = dictionary
+                        elif i['cardSet'] != 'Unknown' and z['cardSet'] != i['cardSet']:
+                            z[f"{i['name'].lower()} ({i['cardSet'].lower()})"] = dictionary
             except IndexError: #Handling known erros
                 continue
             except KeyError:
@@ -126,8 +121,6 @@ for k in cards:
             except Exception as e: #Print errors if new ones arrives
                 print(i['cardId'], type(e).__name__)
                 continue
-            
- 
+
 with open("data/cards.json", "w") as f3:
-    f3.write(json.dumps(z, indent = 4))
-f3.close()
+    json.dump(z, f3, indent=4)
